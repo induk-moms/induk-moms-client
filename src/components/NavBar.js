050ec943cdc7afcf59d195052f07f67d46168cfd
CHO EUN JUNG <0516dmswjd@gmail.com>
2024-08-20 03:13:39 +0900

    [Feat/#7] 네비게이션 클릭 기능

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,29 @@
-import { ActiveGoodIcon, SearchIcon } from '../assets'
+import React, { useState } from 'react'
+import {
+  ActiveGoodIcon,
+  ActiveSearchIcon,
+  GoodIcon,
+  SearchIcon,
+} from '../assets'
 import './NavBar.css'
 
 function NavBar() {
+  const [active, setActive] = useState(1)
+
   return (
     <div id="navbar-wrapper">
       <div id="navbar-btns-wrapper">
-        <ActiveGoodIcon />
-        <SearchIcon />
+        {active === 1 ? (
+          <>
+            <ActiveGoodIcon />
+            <SearchIcon onClick={() => setActive(2)} />
+          </>
+        ) : (
+          <>
+            <GoodIcon onClick={() => setActive(1)} />
+            <ActiveSearchIcon />
+          </>
+        )}
       </div>
     </div>
   )
