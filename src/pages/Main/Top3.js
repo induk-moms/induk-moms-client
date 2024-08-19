e12a9fdbb7b3d2b06a87c3901e0addb8410dcb80
CHO EUN JUNG <0516dmswjd@gmail.com>
2024-08-20 01:32:55 +0900

    [UI/#5] top3 component

---
+++ src/pages/Main/Top3.js
@@ -0,0 +1,32 @@
+import { Top1Img, Top2Img, Top3Img } from '../../assets'
+import './Top3.css'
+
+function Top3() {
+  return (
+    <div id="club-list-top3">
+      <Top1Img id="club_top1" />
+      <div id="club-top23">
+        <Top2Img />
+        <Top3Img />
+      </div>
+      <div id="club_top1_title"></div>
+
+      <div id="club_top23_title">
+        <div className="club-rank-title">
+          <div>Top2</div>
+          <div>CTP</div>
+        </div>
+        <div className="club-rank-title club-top1-wrapper">
+          <div>Top1</div>
+          <div>Induck moms</div>
+        </div>
+        <div className="club-rank-title">
+          <div>Top3</div>
+          <div>CTP</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Top3
