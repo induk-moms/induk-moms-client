53936c08a873d823f8d76be9219ca2edff72f32f
CHO EUN JUNG <0516dmswjd@gmail.com>
2024-08-20 01:30:59 +0900

    [UI/#5] top 3 ui

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,3 +1,4 @@
+import { Top1Img, Top2Img, Top3Img } from '../../assets'
 import Header from './Header'
 import './Main.css'
 
@@ -5,6 +6,28 @@ function Main() {
   return (
     <div>
       <Header />
+      <div id="club-list-top3">
+        <Top1Img id="club_top1" />
+        <div id="club-top23">
+          <Top2Img />
+          <Top3Img />
+        </div>
+        <div id="club_top1_title"></div>
+        <div id="club_top23_title">
+          <div className="club-rank-title">
+            <div>Top2</div>
+            <div>CTP</div>
+          </div>
+          <div className="club-rank-title club-top1-wrapper">
+            <div>Top1</div>
+            <div>Induck moms</div>
+          </div>
+          <div className="club-rank-title">
+            <div>Top3</div>
+            <div>CTP</div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
