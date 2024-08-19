a5387ad32e9b18f0e4b8b6c05ea7c2bed1f476ac
CHO EUN JUNG <0516dmswjd@gmail.com>
2024-08-20 01:53:16 +0900

    [UI/#5] 메인 화면 동아리 리스트

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,14 +1,48 @@
 import Header from './Header'
 import Top3 from './Top3'
 import './Main.css'
+import { ClickIcon } from '../../assets'
 
 function Main() {
   return (
-    <div>
+    <div id="main-page-wrapper">
       <Header />
       <Top3 />
+      <div id="main-list-wrapper">
+        <ListElem />
+        <ListElem />
+        <ListElem />
+        <ListElem />
+        <ListElem />
+        <ListElem />
+      </div>
     </div>
   )
 }
 
 export default Main
+
+function ListElem() {
+  return (
+    <div className="listelem-wrapper">
+      <div className="listelem-rank">4</div>
+      <div className="listelem-info">
+        <div className="listelem-info-wrapper">
+          <div className="listelem-info-title">CTP</div>
+          <div>⭐️ 4.8</div>
+          <div>⭐️ 50</div>
+        </div>
+        <div className="listelem-tag-wrapper">
+          <ListTag tag="개발" />
+          <ListTag tag="해커톤" />
+          <ListTag tag="인하대" />
+        </div>
+      </div>
+      <ClickIcon className="listelem-btn " />
+    </div>
+  )
+}
+
+function ListTag({ tag }) {
+  return <div className="list-tag-wrapper"># {tag}</div>
+}
